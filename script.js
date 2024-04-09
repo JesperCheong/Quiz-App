01a1d417f6d1dfe015c9c773011312c6c4b8be3d
JesperCheong <75248011+JesperCheong@users.noreply.github.com>
2024-04-09 20:19:57 +0000

    Added questions display
    - 10 questions display randomly
    - 4 options for each question display randomly

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 async function fetchQuestions() {
   try {
-  const response = await fetch("https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=multiple");
-  const data = await response.json();
+    const response = await fetch("https://opentdb.com/api.php?amount=10&category=18");
+    const data = await response.json();
     if (data.response_code === 0) {
       console.log("Success: Returned results successfully.");
-      
       questions = data.results.map(item => {
+        //shuffle options
         const tempOptions = [...item.incorrect_answers, item.correct_answer]
-        const randomizedOptions = shuffleArray(tempOptions); //shuffle options
+        const randomizedOptions = shuffleArray(tempOptions);
+        //assign key value pair
         return {
-          question : item.question,
-          answer : item.correct_answer,
-          options : randomizedOptions,
+          question: item.question,
+          answer: item.correct_answer,
+          options: randomizedOptions,
         }
       });
-    } 
+    }
     // handle error according to API documentation for response_code
     else if (data.response_code === 1) {
       console.error("Error: No Results. Could not return results. The API doesn't have enough questions for your query.");
@@ -36,7 +37,7 @@ async function fetchQuestions() {
   }
 }
 
-// Fisher-Yates shuffle to shuffle question options
+// "Fisher-Yates" for shuffling options
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -45,31 +46,64 @@ function shuffleArray(array) {
   return array;
 }
 
-function displayQuestion() {
-  questionDisplay.innerText += questions.question;
-  for (let i = 0; i < questions.option.length; i++) {
-    radioBtn[i].value = questions.option[i]
-    optionDisplays[i].innerHTML = questions.option[i];
-  }
+// need to iterate through "questions" and display question 1 by 1 on html
+async function displayQuestions() {
+  await fetchQuestions();
+  console.log(questions);
+
+  const questionsSection = document.getElementById("questions");
+
+  questions.forEach((question, index) => {
+    //wrapper
+    const questionWrapper = document.createElement("div");
+    questionWrapper.className = "col-md-6 p-3 p-xxl-5";
+    //question number
+    const questionCounter = document.createElement("h5");
+    questionCounter.textContent = `Question ${index + 1}`;
+    questionWrapper.appendChild(questionCounter);
+    //question text
+    const questionText = document.createElement("div");
+    questionText.className = "pb-3";
+    questionText.textContent = question.question;
+    questionWrapper.appendChild(questionText);
+    //options
+    question.options.forEach((option, index) => {
+      const form = document.createElement("div");
+      form.className = "form-check";
+      //create input
+      const input = document.createElement("input");
+      input.className = "form-check-input";
+      input.type = "radio";
+      input.value = option;
+      form.appendChild(input);
+      //create label
+      const label = document.createElement("label");
+      label.className = "form-check-label";
+      label.textContent = option;
+      form.appendChild(label);
+      questionWrapper.appendChild(form);
+    });
+  questionsSection.appendChild(questionWrapper);
+  });
 }
 
-function checkAnswer() {
+/* function checkAnswer() {
   choosenAnswer = document.querySelector("input[name='flexRadioDefault']:checked");
   if (choosenAnswer.value === questions.answer) {
     console.log("correct");
   } else {
     console.log("wrong");
   }
-}
+} */
 
-  
+
 
 let questions = [];
 
-const questionDisplay = document.getElementById("question");
+/* const questionDisplay = document.getElementById("question");
 const radioBtn = document.querySelectorAll(".form-check-input");
-const optionDisplays = document.querySelectorAll(".form-check-label");
+const optionDisplays = document.querySelectorAll(".form-check-label"); */
 
-fetchQuestions();
+displayQuestions();
 
-//https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=multiple
+
